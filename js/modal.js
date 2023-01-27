ad9289eb8c092a30a8bda0c4d85d0024f9df0000
JordanU <42947912+UJordan@users.noreply.github.com>
2023-01-27 11:24:06 -0800

    JS Modal in progress

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,19 @@
 const modalForm = document.querySelector('#modal-form');
 const firstNameInput = document.querySelector('#firstName');
 const lastNameInput = document.querySelector('#lastName');
+const msg = document.querySelector('.msg');
 
 modalForm.addEventListener('submit', onSubmit);
 
 function onSubmit(event) {
     event.preventDefault();
 
-    console.log(firstNameInput.value);
-    console.log(lastNameInput.value);
+    // if(firstNameInput.value === '' || lastNameInput.value === '') {
+    //     msg.classList.add('error')
+    //     msg.innerHMTL = 'Please enter all fields';
+    // } else {
+    //     console.log('success');
+    // }
+
+    console.log(firstNameInput.value, lastNameInput.value);
 }
